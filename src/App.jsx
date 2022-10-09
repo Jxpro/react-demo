2125d6437b79c4c46f774a999c6ea5a5337d3fa7
jokerxin <jxpro@qq.com>
2022-10-10 03:28:50 +0800

    Start the todolist example

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import Hello from './components/Hello';
-import Welcome from './components/Welcome';
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <Hello />
-                <Welcome />
-            </div>
+            <div>App</div>
         );
     }
 }
