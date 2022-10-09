09888cac79228133b6ed4905e174f2e7b09fbd92
jokerxin <jxpro@qq.com>
2022-10-10 03:31:18 +0800

    Start the github search example

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import List from './components/List';
-import Footer from './components/Footer';
 
 export default class App extends Component {
-    state = {
-        todos: [
-            { id: 1, name: '吃饭', done: true },
-            { id: 2, name: '睡觉', done: true },
-            { id: 3, name: '打代码', done: false },
-        ],
-    };
     render() {
         return (
-            <div className="todo-container">
-                <div className="todo-wrap">
-                    <Header addTodo={this.addTodo} />
-                    <List todos={this.state.todos} updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} />
-                    <Footer todos={this.state.todos} updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} />
-                </div>
-            </div>
+            <div>App</div>
         );
     }
-
-    addTodo = todoObj => {
-        this.setState({ todos: [todoObj, ...this.state.todos] });
-    };
-
-    updateTodo = (id, done) => {
-        const newTodos = this.state.todos.map(todo => {
-            if (id.includes(todo.id)) return { ...todo, done };
-            else return todo;
-        });
-        this.setState({ todos: newTodos });
-    };
-
-    deleteTodo = id => {
-        this.setState({ todos: this.state.todos.filter(todo => !id.includes(todo.id)) });
-    };
 }
