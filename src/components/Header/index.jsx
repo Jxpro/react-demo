99d408e40de9d4a4b912dc2926cf0fb344ef0c8f
jokerxin <jxpro@qq.com>
2022-10-11 04:41:28 +0800

    Usage of withRouter function

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default class Header extends Component {
+class Header extends Component {
     render() {
         return (
-            <div className="page-header"><h2>React Router Demo</h2></div>
+            <div className="page-header">
+                <h2>React Router Demo</h2>
+                <button onClick={() => this.props.history.goBack()}>前进</button>&nbsp;
+                <button onClick={() => this.props.history.goForward()}>后退</button>&nbsp;
+                <button onClick={() => this.props.history.go(-2)}>退两步</button>
+            </div>
         );
     }
 }
+
+// withRouter高阶组件，用于包装非路由组件，返回一个新的组
+// 新的组件向非路由组件传递3个属性: history / location / match
+// 从而让非路由组件可以操作路由组件的API，从而实现编程式导航，跳转到指定的路由路径
+export default withRouter(Header);
