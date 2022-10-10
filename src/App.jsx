d22d9c4af86d92cf48c54e86aa20c8eb5ab39bcd
jokerxin <jxpro@qq.com>
2022-10-10 20:09:59 +0800

    Start to learn react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
-import Search from './components/Search';
-import List from './components/List';
 
 export default class App extends Component {
-    state = {
-        users: [],
-        isFirst: true,
-        isLoading: false,
-        err: '',
-    };
-
     render() {
         return (
-            <div className="container" >
-                <Search UpdateAppState={this.UpdateAppState} />
-                <List {...this.state} />
-            </div >
+            <div>App</div>
         );
     }
-
-    UpdateAppState = stateObj => {
-        this.setState(stateObj);
-    };
 }
